bcc5c88303b681d58a2a57238bdac6516e4e0f98
Arun Kumar Tripathi <arun9483@gmail.com>
2022-11-14 12:36:24 +0530

    [ADD] input number validation - client side form validation

--- src/form-validation/MyForm.tsx
+++ src/form-validation/MyForm.tsx
@@ -6,16 +6,32 @@ const MyForm: React.FC<{}> = () => {
   return (
     <div className="form-container">
       <form>
-        <label htmlFor="choose">
-          Would you prefer a banana or cherry? (required)
-        </label>
-        <input
-          id="choose"
-          name="i-like"
-          pattern="[Bb]anana|[Cc]herry"
-          required
-        />
-        <button>Submit</button>
+        <div className="field">
+          <label htmlFor="choose">Would you prefer a banana or a cherry?</label>
+          <input
+            type="text"
+            id="choose"
+            name="i-like"
+            required
+            minLength={6}
+            maxLength={6}
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="number">How many would you like?</label>
+          <input
+            type="number"
+            id="number"
+            name="amount"
+            defaultValue="1"
+            min="1"
+            max="10"
+            step="0.01"
+          />
+        </div>
+        <div className="field">
+          <button>Submit</button>
+        </div>
       </form>
     </div>
   );

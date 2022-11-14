78de9db1fe0b54296357fd106e292227e600a842
Arun Kumar Tripathi <arun9483@gmail.com>
2022-11-14 15:39:21 +0530

    [ADD] add more input controls in constraints - client-side form validation

--- src/form-validation/MyForm.tsx
+++ src/form-validation/MyForm.tsx
@@ -7,27 +7,59 @@ const MyForm: React.FC<{}> = () => {
     <div className="form-container">
       <form>
         <div className="field">
-          <label htmlFor="choose">Would you prefer a banana or a cherry?</label>
+          <fieldset>
+            <legend>
+              Do you have a driver's license?
+              <span aria-label="required">*</span>
+            </legend>
+
+            <input type="radio" required name="driver" id="r1" value="yes" />
+            <label htmlFor="r1">Yes</label>
+            <input type="radio" required name="driver" id="r2" value="no" />
+            <label htmlFor="r2">No</label>
+          </fieldset>
+        </div>
+        <div className="field">
+          <label htmlFor="n1">How old are you?</label>
+
           <input
-            type="text"
-            id="choose"
-            name="i-like"
-            required
-            minLength={6}
-            maxLength={6}
+            type="number"
+            min="12"
+            max="120"
+            step="1"
+            id="n1"
+            name="age"
+            pattern="\d+"
           />
         </div>
         <div className="field">
-          <label htmlFor="number">How many would you like?</label>
+          <label htmlFor="t1">
+            What's your favorite fruit?<span aria-label="required">*</span>
+          </label>
           <input
-            type="number"
-            id="number"
-            name="amount"
-            defaultValue="1"
-            min="1"
-            max="10"
-            step="0.01"
+            type="text"
+            id="t1"
+            name="fruit"
+            list="l1"
+            required
+            pattern="[Bb]anana|[Cc]herry|[Aa]pple|[Ss]trawberry|[Ll]emon|[Oo]range"
           />
+          <datalist id="l1">
+            <option>Banana</option>
+            <option>Cherry</option>
+            <option>Apple</option>
+            <option>Strawberry</option>
+            <option>Lemon</option>
+            <option>Orange</option>
+          </datalist>
+        </div>
+        <div className="field">
+          <label htmlFor="t2">What's your e-mail address?</label>
+          <input type="email" id="t2" name="email" />
+        </div>
+        <div className="field">
+          <label htmlFor="t3">Leave a short message</label>
+          <textarea id="t3" name="msg" maxLength={140} rows={5}></textarea>
         </div>
         <div className="field">
           <button>Submit</button>

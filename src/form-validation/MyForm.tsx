f7026665b0b85e8ea9ee4a88366352a8935652a1
Arun Kumar Tripathi <arun9483@gmail.com>
2022-11-15 10:08:53 +0530

    [ADD] add custom error message without constraints validation API - client-side form validation

--- src/form-validation/MyForm.tsx
+++ src/form-validation/MyForm.tsx
@@ -4,65 +4,60 @@ import './MyForm.css';
 
 const MyForm: React.FC<{}> = () => {
   useEffect(() => {
-    // There are many ways to pick a DOM node; here we get the form itself and the email
-    // input box, as well as the span element into which we will place the error message.
     const form = document.querySelector('form') as HTMLFormElement;
     const email = document.getElementById('mail') as HTMLInputElement;
-    const emailError = document.querySelector(
-      '#mail + span.error'
-    ) as HTMLSpanElement;
+    const error = email.nextElementSibling as HTMLSpanElement;
 
+    // As per the HTML Specification
+    const emailRegExp =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    // Now we can rebuild our validation constraint
+    // Because we do not rely on CSS pseudo-class, we have to
+    // explicitly set the valid/invalid class on our email field
+    const onLoadHandler = (event: Event) => {
+      // Here, we test if the field is empty (remember, the field is not required)
+      // If it is not, we check if its content is a well-formed e-mail address.
+      const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+      email.className = isValid ? 'valid' : 'invalid';
+    };
+
+    // This defines what happens when the user types in the field
     const InputHandler = (event: Event) => {
-      // Each time the user types something, we check if the
-      // form fields are valid.
-
-      if (email.validity.valid) {
-        // In case there is an error message visible, if the field
-        // is valid, we remove the error message.
-        emailError.textContent = ''; // Reset the content of the message
-        emailError.className = 'error'; // Reset the visual state of the message
+      const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+      if (isValid) {
+        email.className = 'valid';
+        error.textContent = '';
+        error.className = 'error';
       } else {
-        // If there is still an error, show the correct error
-        showError();
+        email.className = 'invalid';
       }
     };
 
+    // This defines what happens when the user tries to submit the data
     const submitHandler = (event: Event) => {
-      // if the email field is valid, we let the form submit
-      if (!email.validity.valid) {
-        // If it isn't, we display an appropriate error message
-        showError();
-        // Then we prevent the form from being sent by canceling the event
-        event.preventDefault();
+      event.preventDefault();
+
+      const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+      if (!isValid) {
+        email.className = 'invalid';
+        error.textContent = 'I expect an e-mail, darling!';
+        error.className = 'error active';
+      } else {
+        email.className = 'valid';
+        error.textContent = '';
+        error.className = 'error';
       }
     };
 
+    window.addEventListener('load', onLoadHandler);
     email.addEventListener('input', InputHandler);
-
     form.addEventListener('submit', submitHandler);
-
-    function showError() {
-      if (email.validity.valueMissing) {
-        // If the field is empty,
-        // display the following error message.
-        emailError.textContent = 'You need to enter an e-mail address.';
-      } else if (email.validity.typeMismatch) {
-        // If the field doesn't contain an email address,
-        // display the following error message.
-        emailError.textContent = 'Entered value needs to be an e-mail address.';
-      } else if (email.validity.tooShort) {
-        // If the data is too short,
-        // display the following error message.
-        emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-      }
-
-      // Set the styling appropriately
-      emailError.className = 'error active';
-    }
 
     return () => {
       email.removeEventListener('input', InputHandler);
       form.removeEventListener('submit', submitHandler);
+      window.removeEventListener('load', onLoadHandler);
     };
   }, []);
 
@@ -72,7 +67,7 @@ const MyForm: React.FC<{}> = () => {
         <div className="field">
           <label htmlFor="mail">
             <span>Please enter an email address:</span>
-            <input type="email" id="mail" name="mail" required minLength={8} />
+            <input type="text" id="mail" name="mail" />
             <span className="error" aria-live="polite"></span>
           </label>
         </div>
